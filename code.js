c4718e5b144986347abf8bdb25c0916c75c22447
Christian Acácio <christian4nasc@gmail.com>
2024-10-20 11:59:33 -0300

    order

--- code.js
+++ code.js
@@ -1,4 +1,6 @@
 
+
+
 const preços ={
      strawberry_pie : 12.00,
      chocolate_brownie : 5.50,
@@ -14,11 +16,26 @@ const preços ={
 
 
 let valor = 0;
+let x = 0;
 
 
 function add(produto){  
-    
+
     valor += preços[produto];
-    console.log(valor);
+    x += 1;
+
+
+    let escolhas = document.getElementById("class");
+    let novoparagrafo = document.createElement("p");
+    let texto = document.createTextNode (produto); 
+
+    novoparagrafo.appendChild(texto);
+    escolhas.appendChild(novoparagrafo);
+    
+
+
+
+
+    document.getElementById("total").textContent = "Total: $" + valor.toFixed(2);
 
 }
