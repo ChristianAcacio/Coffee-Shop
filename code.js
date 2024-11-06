d1af52de6f01ed6cee7e3f695ccb2649454fcaf7
Christian <christian4nasc@gmail.com>
2024-11-05 23:12:57 -0300

    Atualização

--- code.js
+++ code.js
@@ -2,15 +2,37 @@
 
 
 const preços ={
-     strawberry_pie : 12.00,
-     chocolate_brownie : 5.50,
-     brazilian_pudim : 5.50,
-     waffle : 7.00,
-     donuts : 6.50,
-     macaron : 3.00,
-     cheesecake : 7.00,
-     chocolate_cake : 7.00,
-     strawberry_cake : 7.00
+    Croissant : 2.00,
+    Brazilian_Coxinha: 2.00,
+    Brazilian_Pastel: 2.00,
+    Empada: 3.00,
+    Natural_Sandwich: 1.00,
+    Chiken_Pie: 2.50,
+    Stuffed_Crepe: 3.00,
+    Cheese_Pizza: 1.00,
+    Pepperoni_Pizza: 1.50,
+
+
+     strawberry_pie : 10.00,
+     chocolate_brownie : 4.50,
+     brazilian_pudim : 4.50,
+     waffle : 6.00,
+     donuts : 3.50,
+     macaron : 1.00,
+     cheesecake : 4.00,
+     chocolate_cake : 4.00,
+     strawberry_cake : 4.00,
+
+
+    American_Coffee : 2.00,
+    Latte_Coffee : 3.00,
+    Macchiato_Coffee : 3.00,
+    White_Chocolate : 3.50,
+    Hot_Chocolate : 3.50,
+    Bublle_Tea : 4.00,
+    Strawberry_Milkshake : 5.50,
+    Chocolate_Milkshake : 5.50,
+    Caramel_Milkshake : 5.50,
 };
 
 
